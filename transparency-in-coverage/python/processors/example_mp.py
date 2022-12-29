a591420d08fa3562c68bf2e0b7f728917bce771b
rl1987 <rl1987@users.noreply.github.com>
2022-12-29 18:40:11 +0800

    Update example_mp.py to make it write to locally hosted Dolt server

--- transparency-in-coverage/python/processors/example_mp.py
+++ transparency-in-coverage/python/processors/example_mp.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# python3 example_mp.py -o "./test_out" -c quest/week0/codes_prelim.csv -n quest/week0/npis.csv -u urls_uniq.txt 
+# python3 example_mp.py -c quest/week0/codes_prelim.csv -n quest/week0/npis.csv
 
 import argparse
 import functools
@@ -12,22 +12,34 @@ import os
 
 from mrfutils import import_csv_to_set, json_mrf_to_csv, InvalidMRF
 
-def perform_task(url, out_dir, code_set, npi_set):
-    out_dir = make_out_dirname(out_dir, url)
-    os.makedirs(out_dir, exist_ok=True)
+import mysql.connector as connector
+
+def perform_task(url, code_set, npi_set):
+    # https://dev.mysql.com/doc/connector-python/en/connector-python-example-connecting.html
+    cnx = connector.connect(user='rl', password='trustno1', host='127.0.0.1', database='quest')
 
     try:
         json_mrf_to_csv(
             loc = url,
-            out_dir = out_dir,
+            cnx = cnx,
             code_filter = code_set,
             npi_filter = npi_set
         )
     except InvalidMRF as e:
         log.critical(e)
+    except Exception as e:
+        log.critical(e)
 
 def make_out_dirname(out_dir, url):
     return os.path.join(out_dir, hashlib.sha1(url.encode('utf-8')).hexdigest())
+
+def get_urls(toc_url):
+    seen_urls = dict()
+
+    for url in get_in_network_links(toc_url):
+        seen_urls[url] = True
+
+    return list(seen_urls.keys())
 
 def main():
     logging.basicConfig()
@@ -35,10 +47,8 @@ def main():
     log.setLevel(logging.DEBUG)
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-o', '--out', default = 'out_dir')
     parser.add_argument('-c', '--codes')
     parser.add_argument('-n', '--npis')
-    parser.add_argument('-u', '--url-list', default="urls_uniq.txt")
 
     args = parser.parse_args()
     out_dir = args.out
@@ -51,13 +61,11 @@ def main():
     else:
         npi_set = None
 
-    in_f = open(args.url_list, "r")
-    urls = in_f.read().strip().split("\n")
-    in_f.close()
- 
+    urls = get_urls('https://antm-pt-prod-dataz-nogbd-nophi-us-east1.s3.amazonaws.com/anthem/2022-12-01_anthem_index.json.gz')
+    urls = urls[:16] # TODO: remove this when it works good on small scale
 
     pool = Pool(16)
-    partial_perform_task = functools.partial(perform_task, out_dir=out_dir, code_set=code_set, npi_set=npi_set)
+    partial_perform_task = functools.partial(perform_task, code_set=code_set, npi_set=npi_set)
     pool.map(partial_perform_task, urls)
 
 if __name__ == "__main__":
