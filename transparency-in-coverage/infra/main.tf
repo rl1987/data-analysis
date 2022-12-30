e62e583181a2ea153bc83f921eaf5a224482ac18
rl1987 <rl1987@users.noreply.github.com>
2022-12-30 09:27:23 +0800

    Use a single CPU-optimised server

--- transparency-in-coverage/infra/main.tf
+++ transparency-in-coverage/infra/main.tf
@@ -22,7 +22,7 @@ resource "digitalocean_droplet" "server" {
   image     = "debian-11-x64"
   name      = "dhdb-quest"
   region    = "sfo3"
-  size      = "s-8vcpu-16gb"
+  size      = "c-8-16gib"
   ssh_keys  = [ for key in data.digitalocean_ssh_keys.keys.ssh_keys: key.fingerprint ]
   user_data = file("provision.sh")
 
@@ -40,33 +40,7 @@ resource "digitalocean_droplet" "server" {
   }
 }
 
-resource "digitalocean_droplet" "devserver" {
-  image     = "debian-11-x64"
-  name      = "dhdb-quest-sqlserver-dev"
-  region    = "sfo3"
-  size      = "s-8vcpu-16gb"
-  ssh_keys  = [ for key in data.digitalocean_ssh_keys.keys.ssh_keys: key.fingerprint ]
-  user_data = file("provision_sql_server.sh")
-
-  connection {
-    host        = self.ipv4_address
-    user        = "root"
-    type        = "ssh"
-    timeout     = "2m"
-    private_key = file("~/.ssh/id_ed25519")
-  }
-
-  provisioner "file" {
-    source      = "~/.dolt"
-    destination = "/root"
-  }
-}
-
 output "server_ip" {
   value = resource.digitalocean_droplet.server.ipv4_address
-}
-
-output "dev_server_ip" {
-  value = resource.digitalocean_droplet.devserver.ipv4_address
 }
 
--- transparency-in-coverage/infra/provision.sh
+++ transparency-in-coverage/infra/provision.sh
@@ -3,13 +3,13 @@
 set -x
 
 apt-get update
-apt-get install -y python3 python3-pip tmux git vim visidata unzip jq
+apt-get install -y python3 python3-pip tmux git vim visidata unzip jq default-libmysqlclient-dev
 
 curl -L https://github.com/dolthub/dolt/releases/latest/download/install.sh > /tmp/install.sh && bash /tmp/install.sh
 dolt config --global --add user.email rimantas@keyspace.lt
 dolt config --global --add user.name "rl1987"
 
-pip3 install --upgrade requests ijson lxml tqdm doltpy
+pip3 install --upgrade requests aiohttp ijson lxml tqdm mysql-connector-python
 
 useradd do-agent --no-create-home --system
 curl -sSL https://repos.insights.digitalocean.com/install.sh -o /tmp/install.sh
@@ -24,5 +24,15 @@ popd || exit
 mkdir /root/src
 
 pushd /root/src || exit
-git clone https://github.com/dolthub/data-analysis.git
+git clone https://github.com/rl1987/data-analysis.git
 popd || exit
+
+curl -fsSL https://deb.nodesource.com/setup_18.x -o /tmp/install_node.sh
+bash /tmp/install_node.sh
+apt-get install -y gcc g++ make nodejs
+
+npm install pm2 -g
+
+sudo pm2 start 'dolt sql-server --data-dir /root/data -H 127.0.0.1 --user=rl --password=trustno1 --loglevel=trace'
+pm2 save
+pm2 startup systemd
