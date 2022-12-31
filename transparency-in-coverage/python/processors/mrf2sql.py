ece864e784b15ed834bff9d0f1931c386675f38f
rl1987 <rl1987@users.noreply.github.com>
2022-12-31 17:52:30 +0800

    Run black to fix formatting

--- transparency-in-coverage/python/processors/mrf2sql.py
+++ transparency-in-coverage/python/processors/mrf2sql.py
@@ -6,25 +6,24 @@ import sys
 from exporters import SQLDumpExporter
 from mrfutils import import_csv_to_set, json_mrf_to_csv, InvalidMRF
 
+
 def main():
     if len(sys.argv) != 3:
         print("Usage:")
         print("{} <input_file_path> <output_file_path>".format(sys.argv[0]))
         return 0
-    
+
     input_file_path = sys.argv[1]
     output_file_path = sys.argv[2]
 
     in_f = open(input_file_path, "r")
-    urls = in_f.read().strip().split('\n')
+    urls = in_f.read().strip().split("\n")
     in_f.close()
 
     script_dir = os.path.dirname(os.path.abspath(__file__))
 
-    code_filter = import_csv_to_set(os.path.join(script_dir,
-                                                 "test/codes.csv"))
-    npi_filter = import_csv_to_set(os.path.join(script_dir,
-                                                "test/npis.csv"))
+    code_filter = import_csv_to_set(os.path.join(script_dir, "test/codes.csv"))
+    npi_filter = import_csv_to_set(os.path.join(script_dir, "test/npis.csv"))
 
     exporter = SQLDumpExporter(output_file_path)
     exporter.start()
@@ -45,6 +44,6 @@ def main():
 
     exporter.finalise()
 
+
 if __name__ == "__main__":
     main()
-
