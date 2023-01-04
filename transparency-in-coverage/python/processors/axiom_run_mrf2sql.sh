42f2d347e02f16ff51fa76daa67e341da20f4052
rl1987 <rl1987@users.noreply.github.com>
2023-01-04 16:41:26 +0800

    Make axiom_run_mrf2sql.sh take TOC URL via CLI

--- transparency-in-coverage/python/processors/axiom_run_mrf2sql.sh
+++ transparency-in-coverage/python/processors/axiom_run_mrf2sql.sh
@@ -11,9 +11,7 @@ axiom-exec --fleet axiom-quest "wget https://bootstrap.pypa.io/get-pip.py && pyt
 axiom-exec --fleet axiom-quest "pip3 install --upgrade requests ijson lxml tqdm aiohttp"
 axiom-exec --fleet axiom-quest "git clone https://github.com/rl1987/data-analysis.git && cd data-analysis/ && git fetch && git checkout axiom"
 
-# python3.9 ~/data-analysis/transparency-in-coverage/python/processors/mrf2sql.py input output
-
-python3 example3.py -u https://antm-pt-prod-dataz-nogbd-nophi-us-east1.s3.amazonaws.com/anthem/2023-01-01_anthem_index.json.gz
+python3 example3.py -u "$1"
 cat in_network_urls.txt | sort | uniq > urls_uniq.txt
 
 axiom-scan urls_uniq.txt -m mrf2sql -o dump.sql
