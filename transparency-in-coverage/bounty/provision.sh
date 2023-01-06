995dd0f62834dc1fbdd9895a4781fd0afe8d6207
rl1987 <rl1987@users.noreply.github.com>
2023-01-06 14:16:10 +0800

    Add Terraform config with provisioning script

---
+++ transparency-in-coverage/bounty/provision.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -x
+
+apt-get update
+apt-get install -y python3 python3-pip tmux git vim visidata unzip jq
+
+curl -L https://github.com/dolthub/dolt/releases/latest/download/install.sh > /tmp/install.sh && bash /tmp/install.sh
+dolt config --global --add user.email rimantas@keyspace.lt
+dolt config --global --add user.name "rl1987"
+
+pip3 install --upgrade requests ijson lxml tqdm aiohttp doltcli
+
+useradd do-agent --no-create-home --system
+curl -sSL https://repos.insights.digitalocean.com/install.sh -o /tmp/install.sh
+bash /tmp/install.sh
+
+mkdir /root/data
+
+pushd /root/data || exit
+dolt clone rl1987/quest
+popd || exit
+
+mkdir /root/src
+
+pushd /root/src || exit
+git clone https://github.com/rl1987/data-analysis.git
+git fetch
+git checkout quest_bounty
+popd || exit
+
+cd /root || exit
+wget "https://raw.githubusercontent.com/pry0cc/axiom/master/interact/axiom-configure"
+sed -i 's/openssh-server//' axiom-configure
+chmod +x axiom-configure
+export HOME=/root
+printf "\n1\nn\ndo\ny\n$(cat do_token.txt)\nsfo3\n\n\n\n\nquest\nbarebones\n\n\n" | ./axiom-configure
