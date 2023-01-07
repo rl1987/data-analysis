5447ad1e8944ca75c941f2117665b47b272c4ff1
rl1987 <rl1987@users.noreply.github.com>
2023-01-07 13:34:35 +0800

    Try to save all output CSVs into directories for each MRF; refrain from converting them to SQL

---
+++ transparency-in-coverage/python/mrfutils/wranglemrf.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+import csv
+from glob import glob
+import os
+import sys
+import uuid
+
+from exceptions import InvalidMRF
+from mrfutils import import_csv_to_set, json_mrf_to_csv
+
+def main():
+    if len(sys.argv) != 3:
+        print("Usage:")
+        print("{} <input_file_path> <output_dir_path>".format(sys.argv[0]))
+        return 0
+
+    input_file_path = sys.argv[1]
+    output_dir_path = sys.argv[2]
+
+    in_f = open(input_file_path, "r")
+    urls = in_f.read().strip().split("\n")
+    in_f.close()
+
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+
+    code_filter = import_csv_to_set(os.path.join(script_dir, "quest/codes.csv"))
+    npi_filter = import_csv_to_set(os.path.join(script_dir, "quest/npis.csv"))
+
+    for url in urls:
+        tries_left = 3
+        u = str(uuid.uuid4())
+
+        out_dir = os.path.join(output_dir_path, u)
+
+        while tries_left > 0:
+            try:
+                print("Starting:", url)
+                json_mrf_to_csv(
+                    url=url,
+                    npi_filter=npi_filter,
+                    code_filter=code_filter,
+                    out_dir=out_dir
+                )
+                print("Done:", url)
+                break
+            except InvalidMRF:
+                print("Invalid MRF at:", url)
+                break
+            except Exception as e:
+                print(e)
+                print("Failed processing MRF at: {}".format(url))
+                tries_left -= 1
+
+if __name__ == "__main__":
+    main()
