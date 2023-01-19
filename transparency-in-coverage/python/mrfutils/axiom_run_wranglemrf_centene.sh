6c08974d9fd3adfc53054b76f62ad8ce99059a61
rl1987 <rl1987@users.noreply.github.com>
2023-01-19 11:03:42 +0800

    fixup! Update Axiom flow for Centene

--- transparency-in-coverage/python/mrfutils/axiom_run_wranglemrf_centene.sh
+++ transparency-in-coverage/python/mrfutils/axiom_run_wranglemrf_centene.sh
@@ -6,9 +6,9 @@ set -x
 
 if [[ -d output_data ]]; then rm -rf output_data; fi
 
-pushd /mnt/volume_sfo3_01/data/quest || exit
+pushd /root/data/quest-small || exit
 dolt checkout main
-dolt remote add upstream dolthub/quest
+dolt remote add upstream dolthub/quest-small
 dolt pull upstream
 dolt push
 popd || exit
@@ -22,13 +22,13 @@ axiom-fleet axiom-quest -i 32
 axiom-exec --fleet axiom-quest "sudo apt-get remove -y python3.8 && sudo apt-get install -y python3.9 python3.9-distutils"
 axiom-exec --fleet axiom-quest "wget https://bootstrap.pypa.io/get-pip.py && python3.9 get-pip.py"
 axiom-exec --fleet axiom-quest "pip3 install --upgrade requests ijson lxml tqdm aiohttp"
-axiom-exec --fleet axiom-quest "git clone https://github.com/rl1987/data-analysis.git && cd data-analysis/ && git fetch && git checkout quest_bounty"
+axiom-exec --fleet axiom-quest "git clone https://github.com/rl1987/data-analysis.git && cd data-analysis/ && git fetch && git checkout quest_small_bounty"
 
 axiom-scan urls.txt -m wranglemrf -o output_data
 axiom-rm -f "axiom-quest*"
 
-cp -R output_data /mnt/volume_sfo3_01/data/quest
-pushd /mnt/volume_sfo3_01/data/quest || exit
+cp -R output_data /root/data/quest-small
+pushd /root/data/quest-small || exit
 dolt checkout main
 dolt pull upstream 
 dolt push
