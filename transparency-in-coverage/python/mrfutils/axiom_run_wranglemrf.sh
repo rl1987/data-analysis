1d0f6954dbd95b10c7085053783f02bbdbc4cd38
rl1987 <rl1987@users.noreply.github.com>
2023-01-19 11:30:45 +0800

    Update Axiom flow for UHC data

--- transparency-in-coverage/python/mrfutils/axiom_run_wranglemrf.sh
+++ transparency-in-coverage/python/mrfutils/axiom_run_wranglemrf.sh
@@ -4,9 +4,9 @@ set -x
 
 if [[ -d output_data ]]; then rm -rf output_data; fi
 
-pushd /mnt/volume_sfo3_01/data/quest || exit
+pushd /root/data/quest-small || exit
 dolt checkout main
-dolt remote add upstream dolthub/quest
+dolt remote add upstream dolthub/quest-small
 dolt pull upstream
 dolt push
 popd || exit
@@ -14,25 +14,25 @@ popd || exit
 cp wranglemrf.json ~/.axiom/modules/
 
 python3 get_pending_mrf_urls.py > pending.txt
-python3 make_chunk.py pending.txt /mnt/volume_sfo3_01/data/quest > urls.txt
+python3 make_chunk.py pending.txt /root/data/quest-small > urls.txt
 
-axiom-fleet axiom-quest -i 32
+axiom-fleet axiom-quest -i 64
 
 axiom-exec --fleet axiom-quest "sudo apt-get remove -y python3.8 && sudo apt-get install -y python3.9 python3.9-distutils"
 axiom-exec --fleet axiom-quest "wget https://bootstrap.pypa.io/get-pip.py && python3.9 get-pip.py"
 axiom-exec --fleet axiom-quest "pip3 install --upgrade requests ijson lxml tqdm aiohttp"
-axiom-exec --fleet axiom-quest "git clone https://github.com/rl1987/data-analysis.git && cd data-analysis/ && git fetch && git checkout quest_bounty"
+axiom-exec --fleet axiom-quest "git clone https://github.com/rl1987/data-analysis.git && cd data-analysis/ && git fetch && git checkout quest_small_bounty"
 
 axiom-scan urls.txt -m wranglemrf -o output_data
 axiom-rm -f "axiom-quest*"
 
-cp -R output_data /mnt/volume_sfo3_01/data/quest
-pushd /mnt/volume_sfo3_01/data/quest || exit
+cp -R output_data /root/data/quest-small
+pushd /root/data/quest-small || exit
 dolt checkout main
 dolt pull upstream 
 dolt push
 
-for table in plans files plans_files codes provider_groups prices prices_provider_groups;do
+for table in file insurer code price_metadata rate npi_rate; do
     for f in $(ls output_data/$table.*); do 
         dolt table import -u "$table" --file-type=csv "$f"
     done
