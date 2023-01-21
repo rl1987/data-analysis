779091f5c61004db93b6b82c94fdc1cac5f95373
rl1987 <rl1987@users.noreply.github.com>
2023-01-21 14:43:27 +0800

    Gather data on how big MRFs are and how long they take to wrangle

--- transparency-in-coverage/python/mrfutils/wranglemrf.py
+++ transparency-in-coverage/python/mrfutils/wranglemrf.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import csv
+from datetime import datetime
 from glob import glob
 import os
 import sys
@@ -11,6 +12,8 @@ import multiprocessing
 
 from exceptions import InvalidMRF
 from mrfutils import import_csv_to_set, json_mrf_to_csv
+
+import requests
 
 def process_url(url):
     output_dir_path = sys.argv[2]
@@ -24,6 +27,16 @@ def process_url(url):
 
     out_dir = os.path.join(output_dir_path, u)
 
+    started_at = datetime.now().timestamp()
+
+    try:
+        resp = requests.head(url, timeout=5.0)
+    except Exception as e:
+        print(e)
+        return
+
+    size = resp.headers.get('Content-Length')
+    
     while tries_left > 0:
         try:
             print("Starting:", url)
@@ -39,13 +52,34 @@ def process_url(url):
             print("Invalid MRF at:", url)
             if os.path.isdir(out_dir):
                 shutil.rmtree(out_dir)
-            break
+            return
         except Exception as e:
             print(e)
             print("Failed processing MRF at: {}".format(url))
             tries_left -= 1
             if os.path.isdir(out_dir):
                 shutil.rmtree(out_dir)
+
+    done_at = datetime.now().timestamp()
+    duration = done_at - started_at
+
+    row = {
+        "url": url,
+        "duration": duration,
+        "size": size,
+        "retries": 3 - tries_left
+    }
+
+    telemetry_started = os.path.isfile(os.path.join(out_dir, "telemetry.csv", "a"))
+
+    out_f = open(os.path.join(out_dir, "telemetry.csv"), "a", encoding="utf-8")
+
+    csv_writer = csv.DictWriter(out_f, fieldnames = list(row.keys()), lineterminator="\n")
+    if not telemetry_started:
+        csv_writer.writeheader()
+    csv_writer.writerow(row)
+
+    out_f.close()
 
 def main():
     if len(sys.argv) != 3:
