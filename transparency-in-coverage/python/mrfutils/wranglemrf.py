27cff7c18b239e9c3e1e4f72585cf1033ded31f4
rl1987 <rl1987@users.noreply.github.com>
2023-01-21 14:30:21 +0800

    Make wranglemrf.py multiprocessed - one subprocess per CPU core

--- transparency-in-coverage/python/mrfutils/wranglemrf.py
+++ transparency-in-coverage/python/mrfutils/wranglemrf.py
@@ -6,9 +6,46 @@ import os
 import sys
 import shutil
 import uuid
+from multiprocessing import Pool
+import multiprocessing
 
 from exceptions import InvalidMRF
 from mrfutils import import_csv_to_set, json_mrf_to_csv
+
+def process_url(url):
+    output_dir_path = sys.argv[2]
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+
+    code_filter = import_csv_to_set(os.path.join(script_dir, "quest/codes.csv"))
+    npi_filter = import_csv_to_set(os.path.join(script_dir, "quest/npis.csv"))
+
+    tries_left = 3
+    u = str(uuid.uuid4())
+
+    out_dir = os.path.join(output_dir_path, u)
+
+    while tries_left > 0:
+        try:
+            print("Starting:", url)
+            json_mrf_to_csv(
+                url=url,
+                npi_filter=npi_filter,
+                code_filter=code_filter,
+                out_dir=out_dir
+            )
+            print("Done:", url)
+            break
+        except InvalidMRF:
+            print("Invalid MRF at:", url)
+            if os.path.isdir(out_dir):
+                shutil.rmtree(out_dir)
+            break
+        except Exception as e:
+            print(e)
+            print("Failed processing MRF at: {}".format(url))
+            tries_left -= 1
+            if os.path.isdir(out_dir):
+                shutil.rmtree(out_dir)
 
 def main():
     if len(sys.argv) != 3:
@@ -17,43 +54,13 @@ def main():
         return 0
 
     input_file_path = sys.argv[1]
-    output_dir_path = sys.argv[2]
 
     in_f = open(input_file_path, "r")
     urls = in_f.read().strip().split("\n")
     in_f.close()
 
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-
-    code_filter = import_csv_to_set(os.path.join(script_dir, "quest/codes.csv"))
-    npi_filter = import_csv_to_set(os.path.join(script_dir, "quest/npis.csv"))
-
-    for url in urls:
-        tries_left = 3
-        u = str(uuid.uuid4())
-
-        out_dir = os.path.join(output_dir_path, u)
-
-        while tries_left > 0:
-            try:
-                print("Starting:", url)
-                json_mrf_to_csv(
-                    url=url,
-                    npi_filter=npi_filter,
-                    code_filter=code_filter,
-                    out_dir=out_dir
-                )
-                print("Done:", url)
-                break
-            except InvalidMRF:
-                print("Invalid MRF at:", url)
-                shutil.rmtree(out_dir)
-                break
-            except Exception as e:
-                print(e)
-                print("Failed processing MRF at: {}".format(url))
-                tries_left -= 1
-                shutil.rmtree(out_dir)
+    pool = Pool(multiprocessing.cpu_count())
+    pool.map(process_url, urls)
 
 if __name__ == "__main__":
     main()
