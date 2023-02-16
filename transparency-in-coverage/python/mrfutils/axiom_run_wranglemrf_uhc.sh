0048495d29031b5a5f5f6f3241f9c465ca55c3d6
rl1987 <rl1987@users.noreply.github.com>
2023-02-16 10:23:09 +0800

    Update axiom_run_wranglemrf_uhc.sh

--- transparency-in-coverage/python/mrfutils/axiom_run_wranglemrf_uhc.sh
+++ transparency-in-coverage/python/mrfutils/axiom_run_wranglemrf_uhc.sh
@@ -5,9 +5,9 @@ set -x
 if [[ -d output_data ]]; then rm -rf output_data; fi
 if [[ -d raw_output_data ]]; then rm -rf raw_output_data; fi
 
-pushd /root/data/quest-v3 || exit
+pushd /root/data/quest-v4 || exit
 dolt checkout main
-dolt remote add upstream dolthub/quest-v3
+dolt remote add upstream dolthub/quest-v4
 dolt pull upstream
 dolt push
 popd || exit
@@ -15,14 +15,14 @@ popd || exit
 cp wranglemrf.json ~/.axiom/modules/
 
 python3 get_pending_mrf_urls.py | anew > pending.txt
-python3 make_chunk_uhc.py pending.txt /root/data/quest-v3 > urls.txt
+python3 make_chunk_uhc.py pending.txt /root/data/quest-v4 > urls.txt
 
 axiom-fleet axiom-quest -i 64
 
 axiom-exec --fleet axiom-quest "sudo apt-get remove -y python3.8 && sudo apt-get install -y python3.9 python3.9-distutils"
 axiom-exec --fleet axiom-quest "wget https://bootstrap.pypa.io/get-pip.py && python3.9 get-pip.py"
 axiom-exec --fleet axiom-quest "pip3 install --upgrade requests ijson lxml tqdm aiohttp"
-axiom-exec --fleet axiom-quest "git clone https://github.com/rl1987/data-analysis.git && cd data-analysis/ && git fetch && git checkout quest_v3_bounty"
+axiom-exec --fleet axiom-quest "git clone https://github.com/rl1987/data-analysis.git && cd data-analysis/ && git fetch && git checkout quest_v4_bounty"
 
 axiom-scan urls.txt -m wranglemrf -o raw_output_data
 axiom-rm -f "axiom-quest*"
@@ -30,13 +30,13 @@ axiom-rm -f "axiom-quest*"
 mkdir output_data
 python3 unify_csv_files.py raw_output_data output_data
 
-cp -R output_data /root/data/quest-v3
-pushd /root/data/quest-v3 || exit
+cp -R output_data /root/data/quest-v4
+pushd /root/data/quest-v4 || exit
 dolt checkout main
 dolt pull upstream 
 dolt push
 
-for table in file insurer code price_metadata rate npi_rate file_rate; do
+for table in file insurer code price_metadata rate tin file_rate tin_rate_file npi_tin; do
     time -p dolt table import -u "$table" "output_data/$table.csv"
 done
 
