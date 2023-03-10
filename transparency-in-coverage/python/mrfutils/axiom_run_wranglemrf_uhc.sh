1f95a8b1b085caa113dde01c1924724ddedd8d60
rl1987 <rl1987@users.noreply.github.com>
2023-03-10 11:38:56 +0700

    Start reworking code for new bounty

--- transparency-in-coverage/python/mrfutils/axiom_run_wranglemrf_uhc.sh
+++ transparency-in-coverage/python/mrfutils/axiom_run_wranglemrf_uhc.sh
@@ -5,24 +5,23 @@ set -x
 if [[ -d output_data ]]; then rm -rf output_data; fi
 if [[ -d raw_output_data ]]; then rm -rf raw_output_data; fi
 
-pushd /root/data/quest-v4 || exit
+pushd /root/data/hospital-prices-allpayers || exit
 dolt checkout main
-dolt remote add upstream dolthub/quest-v4
+dolt remote add upstream dolthub/hospital-prices-allpayers
 dolt pull upstream
 dolt push
 popd || exit
 
 cp wranglemrf.json ~/.axiom/modules/
 
-python3 get_pending_mrf_urls.py | anew > pending.txt
-python3 make_chunk_uhc.py pending.txt /root/data/quest-v4 > urls.txt
+python3 make_chunk_uhc.py  > urls.txt
 
 axiom-fleet axiom-quest -i 64
 
 axiom-exec --fleet axiom-quest "sudo apt-get remove -y python3.8 && sudo apt-get install -y python3.9 python3.9-distutils"
 axiom-exec --fleet axiom-quest "wget https://bootstrap.pypa.io/get-pip.py && python3.9 get-pip.py"
 axiom-exec --fleet axiom-quest "pip3 install --upgrade requests ijson lxml tqdm aiohttp"
-axiom-exec --fleet axiom-quest "git clone https://github.com/rl1987/data-analysis.git && cd data-analysis/ && git fetch && git checkout quest_v4_bounty"
+axiom-exec --fleet axiom-quest "git clone https://github.com/rl1987/data-analysis.git && cd data-analysis/ && git fetch && git checkout allpayers"
 
 axiom-scan urls.txt -m wranglemrf -o raw_output_data
 axiom-rm -f "axiom-quest*"
@@ -30,20 +29,20 @@ axiom-rm -f "axiom-quest*"
 mkdir output_data
 python3 unify_csv_files.py raw_output_data output_data
 
-cp -R output_data /root/data/quest-v4
-pushd /root/data/quest-v4 || exit
+cp -R output_data /root/data/hospital-prices-allpayers
+pushd /root/data/hospital-prices-allpayers || exit
 dolt checkout main
 dolt pull upstream 
 dolt push
 
-for table in file insurer code price_metadata rate tin tin_rate_file npi_tin; do
+for table in file insurer code price_metadata rate tin tin_rate_file npi_tin toc toc_insurer; do
     for f in $(ls output_data/$table.*); do 
         time -p nice -n -10 dolt table import -u "$table" --file-type=csv "$f"
     done
 done
 
 branch_name=$(date --rfc-3339=seconds | sed 's/://g' | sed 's/\s/__/g' | sed 's/-/_/g' | cut -f1 -d '+')
-dolt checkout -b "$branch_name" vqbnk0gdv2lhsuvfoc142bg4q8023i1t
+dolt checkout -b "$branch_name" 1b00ore43mnan035hjjducplj336pu25
 dolt add .
 dolt commit -m "UHC data"
 dolt push -u origin "$branch_name"
--- transparency-in-coverage/python/mrfutils/make_chunk_uhc.py
+++ transparency-in-coverage/python/mrfutils/make_chunk_uhc.py
@@ -2,57 +2,22 @@
 
 import sys
 
-import doltcli as dolt
 import requests
 
-CHUNK_SIZE = 128
-
 def main():
-    if len(sys.argv) != 3:
-        print("Usage:")
-        print("{} <pending_urls_file> <dolt_db_dir>".format(sys.argv[0]))
-        return
-
-    pending_urls_file = sys.argv[1]
-    dolt_db_dir = sys.argv[2]
-
-    exclude = set()
-
-    in_f = open(pending_urls_file, "r")
-    for url in in_f.read().strip().split("\n"):
-        exclude.add(url)
-    in_f.close()
-
-    db = dolt.Dolt(dolt_db_dir)
-    sql = 'SELECT DISTINCT(url) FROM file WHERE url IS NOT NULL;'
-    
-    try:
-        res = db.sql(sql, result_format="json")
-        for row in res['rows']:
-            exclude.add(row.get('url)'))
-    except:
-        pass
-
     candidates = []
 
     resp = requests.get("https://transparency-in-coverage.uhc.com/api/v1/uhc/blobs/")
 
     for blob_dict in resp.json().get("blobs"):
         url = blob_dict.get("downloadUrl")
-        if not "in-network" in url:
-            continue
-        size = blob_dict.get("size")
+        if "index" in url:
+            candidates.append(url)
 
-        if not url in exclude:
-            candidates.append((url, size))
-    
     candidates = sorted(candidates, key=lambda t: t[1], reverse=True)
     
-    if len(candidates) > CHUNK_SIZE:
-        candidates = candidates[:CHUNK_SIZE]
-
-    for c in candidates:
-        print(c[0])
+    for url in candidates:
+        print(url)
 
 if __name__ == "__main__":
     main()
