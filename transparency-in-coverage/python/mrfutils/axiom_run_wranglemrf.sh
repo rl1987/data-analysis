576b3b6fe0292841065c83e1a31430150db19d1c
rl1987 <rl1987@users.noreply.github.com>
2023-03-11 16:58:49 +0700

    Make make_chunk_uhc.py disregard pending submissions

--- transparency-in-coverage/python/mrfutils/axiom_run_wranglemrf.sh
+++ transparency-in-coverage/python/mrfutils/axiom_run_wranglemrf.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# FIXME: code duplication with axiom_run_wranglemrf.sh
-
 set -x
 
 if [[ -d output_data ]]; then rm -rf output_data; fi
@@ -15,17 +13,18 @@ popd || exit
 
 cp wranglemrf.json ~/.axiom/modules/
 
-python3 centene_get_mrf_urls.py
-
-axiom-fleet axiom-quest -i 16
+axiom-fleet axiom-quest -i $1
 
 axiom-exec --fleet axiom-quest "sudo apt-get remove -y python3.8 && sudo apt-get install -y python3.9 python3.9-distutils"
 axiom-exec --fleet axiom-quest "wget https://bootstrap.pypa.io/get-pip.py && python3.9 get-pip.py"
 axiom-exec --fleet axiom-quest "pip3 install --upgrade requests ijson lxml tqdm aiohttp"
 axiom-exec --fleet axiom-quest "git clone https://github.com/rl1987/data-analysis.git && cd data-analysis/ && git fetch && git checkout allpayers"
 
-axiom-scan urls.txt -m wranglemrf -o output_data
+axiom-scan urls.txt -m wranglemrf -o raw_output_data
 axiom-rm -f "axiom-quest*"
+
+mkdir output_data
+python3 unify_csv_files.py raw_output_data output_data
 
 cp -R output_data /root/data/hospital-prices-allpayers
 pushd /root/data/hospital-prices-allpayers || exit
@@ -33,16 +32,16 @@ dolt checkout main
 dolt pull upstream 
 dolt push
 
-for table in file insurer code price_metadata rate tin tin_rate_file npi_tin toc toc_insurer; do
+for table in file code rate_metadata rate tin tin_rate_file npi_tin; do
     for f in $(ls output_data/$table.*); do 
         time -p nice -n -10 dolt table import -u "$table" --file-type=csv "$f"
     done
 done
 
-branch_name="centene"
-dolt checkout -b "$branch_name" 1b00ore43mnan035hjjducplj336pu25
+branch_name="$2"
+dolt checkout -b "$branch_name" n0htpqrud2rf2ft5pjnk0lrcd9bh0br3
 dolt add .
-dolt commit -m "Centene data"
+dolt commit -m "$branch_name data"
 dolt push -u origin "$branch_name"
 dolt checkout main
 rm -rf output_data
--- transparency-in-coverage/python/mrfutils/make_chunk_uhc.py
+++ transparency-in-coverage/python/mrfutils/make_chunk_uhc.py
@@ -2,22 +2,51 @@
 
 import sys
 
+import doltcli as dolt
 import requests
 
+CHUNK_SIZE = 128
+
 def main():
+    if len(sys.argv) != 2:
+        print("Usage:")
+        print("{} <dolt_db_dir>".format(sys.argv[0]))
+        return
+
+    dolt_db_dir = sys.argv[1]
+
+    exclude = set()
+
+    db = dolt.Dolt(dolt_db_dir)
+    sql = 'SELECT DISTINCT(url) FROM file WHERE url IS NOT NULL;'
+    
+    try:
+        res = db.sql(sql, result_format="json")
+        for row in res['rows']:
+            exclude.add(row.get('url)'))
+    except:
+        pass
+
     candidates = []
 
     resp = requests.get("https://transparency-in-coverage.uhc.com/api/v1/uhc/blobs/")
 
     for blob_dict in resp.json().get("blobs"):
         url = blob_dict.get("downloadUrl")
-        if "index" in url:
-            candidates.append(url)
+        if not "in-network" in url:
+            continue
+        size = blob_dict.get("size")
 
+        if not url in exclude:
+            candidates.append((url, size))
+    
     candidates = sorted(candidates, key=lambda t: t[1], reverse=True)
     
-    for url in candidates:
-        print(url)
+    if len(candidates) > CHUNK_SIZE:
+        candidates = candidates[:CHUNK_SIZE]
+
+    for c in candidates:
+        print(c[0])
 
 if __name__ == "__main__":
     main()
