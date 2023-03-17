0151ec9b96b335651a259cea7e9c05ba85fbf704
rl1987 <rl1987@users.noreply.github.com>
2023-03-17 19:01:31 +0700

    Update script to get Centene MRF URLs

--- transparency-in-coverage/python/mrfutils/centene_get_mrf_urls.py
+++ transparency-in-coverage/python/mrfutils/centene_get_mrf_urls.py
@@ -5,7 +5,23 @@ from urllib.parse import urljoin
 import requests
 from lxml import html
 
-from idxutils import gen_in_network_links
+from mrfutils.helpers import JSONOpen
+
+def gen_in_network_links(index_loc,):
+    """
+    Gets in-network files from index.json files
+    :param index.json URL:
+    """
+    with JSONOpen(index_loc) as f:
+        count = 0
+        parser = ijson.parse(f, use_float=True)
+        for prefix, event, value in parser:
+            if (
+                prefix.endswith('location')
+                and event == 'string'
+                and 'in-network' in value
+            ):
+                yield value
 
 def main():
     out_f = open("urls.txt", "w")
